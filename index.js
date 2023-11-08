6d2ae2ef87b37beb59106608dd6ee205f28855ad
gimenados <gimenasande1@gmail.com>
2023-11-08 12:00:26 -0300

    Ejercicio 1

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let cantidadDeGatos = 10; //Declaro variable y le doy un valor
 
 //i = 1 Porque arranco del 1 ; i menor o igual a cantidadDeGatos porque tienen q ser numeros menores a 10  
 for (let i = 1; i <= cantidadDeGatos; i++) { 
-//Declaro la variable emoji y le doy su valor 
+//Declaro la variable emoji y le doy su valor
     let emoji = i % 3 === 1 ? "😺" : (i % 3 === 2 ? "😸" : "😹");
-    console.log("Gato #" + i + ": " + emoji);
+    console.log(`Gato #${i}: ${emoji}`)
 }
+
