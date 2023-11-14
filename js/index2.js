046191a68ec8669ed0c513e6eabafe73fd7008ec
gimenados <gimenasande1@gmail.com>
2023-11-14 18:31:37 -0300

    Ejercicio 2

--- js/index2.js
+++ js/index2.js
@@ -3,10 +3,8 @@ let cantidadDePasos = 4;
 
 for (var i = 1; i <= cantidadDeGatos; i++) {
     
-    let pasosEmoji = "🐾".repeat(cantidadDePasos);
+    let pasosEmoji = "🐾".repeat(cantidadDePasos); //Porq yo quiero que se repita la cantidad de veces que dice la variable usando repeat()
     let gatosEmoji = "🐈".repeat(cantidadDeGatos);
-    
    
    console.log(`Gatos #${i} ${pasosEmoji} ${gatosEmoji}`);
-
 }
--- js/index3.js
+++ js/index3.js
@@ -1,7 +1,7 @@
 let cantidadDeGatos = 10;
 let cantidadDePasos = 4;
 
-for (var i = 1; i <= cantidadDeGatos; i++) {
+for (var i = 1; i <= cantidadDeGatos; i++) { //Yo se que los dos gatos 1 y 2 son divisibles a 2 entonces por cada diferencia de resto a cada uno le igualo 2
     if (i % 2 === 1 ){
         emoji = "🐈";
     } else if (i % 2 === 0 ){
