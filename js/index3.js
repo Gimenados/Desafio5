de2d9e1c30991f66d1fb95feff4434ff3257d5c2
gimenados <gimenasande1@gmail.com>
2023-11-15 16:47:20 -0300

    Repaso

--- js/index3.js
+++ js/index3.js
@@ -1,7 +1,7 @@
 let cantidadDeGatos = 10;
 let cantidadDePasos = 4;
 
-for (var i = 1; i <= cantidadDeGatos; i++) { //Yo se que los dos gatos 1 y 2 son divisibles a 2 entonces por cada diferencia de resto a cada uno le igualo 2
+for (var i = 1; i <= cantidadDeGatos; i++) { //Yo se que los dos gatos 1 y 2 son divisibles a 2 entonces por cada diferencia de resto a cada uno le igualo pa los resultados 1 y 0
     if (i % 2 === 1 ){
         emoji = "🐈";
     } else if (i % 2 === 0 ){
